55e866ee2b75a765f30e132e4549b101ea51ed70
sachinadkar <sachin.adkarr@gmail.com>
2017-12-26 17:04:51 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -14,13 +14,21 @@ app.post('/todos', (req, res) => {
  console.log(req.body);
  var todo = new Todo({
    text: req.body.text
- });
+ });	
  todo.save().then((doc)=>{
    res.send(doc)
  },(e)=>{
 res.status(400).send(e);
  });
 });
+
+app.get('/todos', (req, res)=>{
+  Todo.find().then((todos)=>{          //find all the todos
+    res.send({todos});
+  },(e)=>{
+res.status(400).send(e);
+  })
+})
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
