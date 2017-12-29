0eca0bae9c41ac1c3daa7d32a1ff70a5653c7c57
sachinadkar <sachin.adkarr@gmail.com>
2017-12-29 10:43:42 +0530

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -1,34 +1,32 @@
-require('./config/config')
+require('./config/config');
 
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
-var { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/users');
 
 var app = express();
-//The process.env is set app is running on heroku, else it will run locally on port 3000
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  console.log(req.body);
   var todo = new Todo({
     text: req.body.text
   });
+
   todo.save().then((doc) => {
-    res.send(doc)
+    res.send(doc);
   }, (e) => {
     res.status(400).send(e);
   });
 });
 
 app.get('/todos', (req, res) => {
-  //find all the todos
   Todo.find().then((todos) => {
     res.send({ todos });
   }, (e) => {
@@ -36,82 +34,86 @@ app.get('/todos', (req, res) => {
   });
 });
 
-
-//Pass the query using the query string
-
 app.get('/todos/:id', (req, res) => {
-  //Params has the key value pairs, where key is url and value is whatever we give
   var id = req.params.id;
-  if (!ObjectID.isValid(id)) {
-    res.status(404).send();
-  }
-  Todo.findById(id).then((todo) => {
-    if (!todo) {
-      console.log("Todo not found");
-      res.status(404).send();
-    }
-    else {
-
-      res.send({ todo });
-    }
-  }
-  ).catch((e) => res.status(404).send());
-});
-
-
-app.delete('/todos/:id', (req, res) => {
-  //get id
-
-  var id = req.params.id;
-
-  //Validate the id --> not valid? return 404
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
-  // remove todo by id
-  Todo.findByIdAndRemove(id).then((todo) => {
- 
-    //if no doc 404
+  Todo.findById(id).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
-    //succes
-    //if doc return it
-    res.send({todo});
-  }).catch((e) => res.status(404).send()); //error 404
+
+    res.send({ todo });
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
-app.patch('/todos/:id', (req, res)=>{
+app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
-  //pick takes two arguments, one is req.body and other is the properties that need to be pulled off
-  var body = _.pick(req.body, ['text','completed']);  
- 
+
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
-  if(_.isBoolean(body.completed) && body.completed){
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({ todo });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
     body.completedAt = new Date().getTime();
-  }else{
+  } else {
     body.completed = false;
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id,{$set: body},{new: true}).then((todo)=>{
-if(!todo){
-  return res.status(404).send();
-}
-res.send({todo});
-  }).then((e)=>{
-    res.status(404).send();
-  });
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
 
+    res.send({ todo });
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+
+    return user.generateAuthToken();
+   
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
 });
 
 app.listen(port, () => {
-  console.log('Started on port', port);
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = { app };
