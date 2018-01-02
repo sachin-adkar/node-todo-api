87cfc570a4b143c5f1f66bad8eeb8ec0adcb9de1
sachinadkar <sachin.adkarr@gmail.com>
2018-01-02 20:15:22 +0530

    Update config file to use environment variables from json

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,14 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];  //grab the property
+
+  // Objetakes an object gets all the keys and return as an array 
+  Object.keys(envConfig).forEach((key)=>{
+    process.env[key] = envConfig[key];
+  });
 }
+
+
--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -1,35 +1,36 @@
-const {ObjectID} = require('mongodb');
+const { ObjectID } = require('mongodb');
 const jwt = require('jsonwebtoken');
 
-const {Todo} = require('./../../models/todo');
-const {User} = require('./../../models/users');
+const { Todo } = require('./../../models/todo');
+const { User } = require('./../../models/users');
 
 const userOneId = new ObjectID();
 const userTwoId = new ObjectID();
 const users = [
   {
-  _id: userOneId,
-  email: 'sachin@example.com',
-  password: 'userOnePass',
-  tokens: [{
-    access: 'auth',
-    token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
-  }]
-}, {
-  _id: userTwoId,
-  email: 'glen@example.com',
-  password: 'userTwoPass',
-  tokens: [{
-    access: 'auth',
-    token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
-  }]
-}];
+    _id: userOneId,
+    email: 'sachin@example.com',
+    password: 'userOnePass',
+    tokens: [{
+      access: 'auth',
+      token: jwt.sign({ _id: userOneId, access: 'auth' }, process.env.JWT_SECRET).toString()
+    }]
+  }, 
+  {
+    _id: userTwoId,
+    email: 'glen@example.com',
+    password: 'userTwoPass',
+    tokens: [{
+      access: 'auth',
+      token: jwt.sign({ _id: userTwoId, access: 'auth' }, process.env.JWT_SECRET).toString()
+    }]
+  }];
 
 const todos = [{
   _id: new ObjectID(),
   _creator: userOneId,
   text: 'First test todo'
-},{
+}, {
   _id: new ObjectID(),
   text: 'Second test todo',
   _creator: userTwoId,
@@ -38,30 +39,30 @@ const todos = [{
 }];
 
 const populateTodos = (done) => {
-  
-    
+
+
   Todo.remove({})
-  .then(() => {
-    return Todo.insertMany(todos);
-  })
-  .then(() => done())
-  .catch((err)=>{
-      
-  })
+    .then(() => {
+      return Todo.insertMany(todos);
+    })
+    .then(() => done())
+    .catch((err) => {
+
+    })
 };
 
 const populateUsers = (done) => {
   User.remove({})
-  .then(() => {
-    var userOne = new User(users[0]).save();
-    var userTwo = new User(users[1]).save();
+    .then(() => {
+      var userOne = new User(users[0]).save();
+      var userTwo = new User(users[1]).save();
 
-    return Promise.all([userOne, userTwo])
-  })
-  .then(() => done())
-  .catch((err)=>{
+      return Promise.all([userOne, userTwo])
+    })
+    .then(() => done())
+    .catch((err) => {
 
-  });
+    });
 };
 
-module.exports = {todos, populateTodos, users, populateUsers};
+module.exports = { todos, populateTodos, users, populateUsers };
