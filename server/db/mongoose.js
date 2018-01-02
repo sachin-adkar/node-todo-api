7d0efaf0b5e165dbaa590513e330fcfd6414b791
sachinadkar <sachin.adkarr@gmail.com>
2018-01-02 23:16:17 +0530

    Set heroku URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI,{useMongoClient:true});
+mongoose.connect((process.env.MONGODB_URI || 'mongodb://localhost/27017/TodoApp'), {useMongoCLient: true});
 
 module.exports = { mongoose };
